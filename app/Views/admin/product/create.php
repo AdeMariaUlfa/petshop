b609b5d4ab888a119329e59029569ff73253a299
AdeMariaUlfa <ademariaulfaa@gmail.com>
2022-11-28 20:35:35 +0700

    css gabisa 2

--- app/Views/admin/product/create.php
+++ app/Views/admin/product/create.php
@@ -14,18 +14,19 @@
                     <label for="product">Product</label>
                     <input type="text" class="form-control" name="produk" required="required">
                 </div>
-                <div class="form-group">
+                 <div class="mb-3">
+                        <label for="gambar" class="form-label">Gambar</label>
+                        <input type="file" class="form-control" id="gambar" name="gambar" required="required">
+                    </div>
+                <!-- <div class="form-group">
                     <label for="gambar">Gambar</label>
                     <input type="text" class="form-control" name="gambar" required="required">
-                </div>
+                </div> -->
                 <div class="form-group">
                     <label for="harga">Harga</label>
                     <input type="number" class="form-control" name="harga" required="required">
                 </div>
-                <!-- <div class="mb-3">
-                        <label for="gambar" class="form-label">Gambar</label>
-                        <input type="file" class="form-control" id="gambar" name="gambar" required="required">
-                    </div> -->
+               
                 <div class="form-group">
                     <input type="submit" value="Simpan" class="btn btn-info" />
                 </div>
